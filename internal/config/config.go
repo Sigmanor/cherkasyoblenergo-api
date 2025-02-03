c3a6640e7f28acf27414dd05059353c2f5574d58
sigmanor <sigmanor@gmail.com>
2025-02-03 15:02:20 +0200

    feat: add internal config package

---
+++ internal/config/config.go
@@ -0,0 +1,35 @@
+package config
+
+import (
+	"github.com/spf13/viper"
+)
+
+const version = "1.0.0"
+
+type Config struct {
+	APP_VERSION   string
+	DBHost        string `mapstructure:"DB_HOST"`
+	DBPort        string `mapstructure:"DB_PORT"`
+	DBUser        string `mapstructure:"DB_USER"`
+	DBPassword    string `mapstructure:"DB_PASSWORD"`
+	DBName        string `mapstructure:"DB_NAME"`
+	AdminPassword string `mapstructure:"ADMIN_PASSWORD"`
+	SERVER_PORT   string `mapstructure:"SERVER_PORT"`
+}
+
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName(".env")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	config.APP_VERSION = version
+	return
+}
