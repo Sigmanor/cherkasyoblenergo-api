2fb32606a67d7ab0192821d27c3eb257b1d6b592
sigmanor <sigmanor@gmail.com>
2025-02-03 15:02:46 +0200

    feat: add API key authentication middleware

---
+++ internal/middleware/auth.go
@@ -0,0 +1,34 @@
+package middleware
+
+import (
+	"cherkasyoblenergo-api/internal/models"
+
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
+)
+
+func APIKeyAuth(db *gorm.DB) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		if c.Path() == "/cherkasyoblenergo/api/generate-api-key" {
+			return c.Next()
+		}
+
+		apiKey := c.Get("X-API-Key")
+		if apiKey == "" {
+			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+				"error": "API key is missing",
+			})
+		}
+
+		var key models.APIKey
+		if err := db.Where("key = ?", apiKey).First(&key).Error; err != nil {
+			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+				"error": "Invalid API key",
+			})
+		}
+
+		c.Locals("api_key", key)
+
+		return c.Next()
+	}
+}
