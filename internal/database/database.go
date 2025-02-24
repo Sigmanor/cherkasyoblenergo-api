d63c52ad4829c26987f261f62ced72f4fac9b2d3
sigmanor <sigmanor@gmail.com>
2025-02-24 17:24:03 +0200

    refactor: extract DSN creation into a separate function for improved readability

--- internal/database/database.go
+++ internal/database/database.go
@@ -9,14 +9,16 @@ import (
 	"gorm.io/gorm"
 )
 
-func ConnectDB(cfg config.Config) (*gorm.DB, error) {
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+func createDSN(cfg config.Config) string {
+	return fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
 		cfg.DBHost, cfg.DBUser, cfg.DBPassword, cfg.DBName, cfg.DBPort)
+}
 
+func ConnectDB(cfg config.Config) (*gorm.DB, error) {
+	dsn := createDSN(cfg)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, err
 	}
-
 	return db, nil
 }
