e9f7f25da493d2fba4ff63f34f248a5ade7d3db6
sigmanor <sigmanor@users.noreply.github.com>
2025-11-22 22:25:10 +0200

    test(parser): add schedule parsing cases

--- internal/parser/parser_test.go
+++ internal/parser/parser_test.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
+	"cherkasyoblenergo-api/internal/models"
 )
 
 func TestParseScheduleFromParagraphs_ToleratesExtraDotAndSpaces(t *testing.T) {
@@ -28,4 +30,25 @@ func TestParseScheduleFromParagraphs_ToleratesExtraDotAndSpaces(t *testing.T) {
 
 func TestNormalizeTimeRanges_TrimsTrailingSeparators(t *testing.T) {
 	assert.Equal(t, "02:00 - 04:00", normalizeTimeRanges(" 02:00 - 04:00, "))
+}
+
+func TestParseScheduleFromParagraphs_HandlesDivBlocks(t *testing.T) {
+	html := `
+<div data-block="true"><div>1.1 00:00 - 02:00, 06:00 - 08:00</div></div>
+<div data-block="true"><div>2.1 08:00 - 10:00, 13:00 - 15:00</div></div>
+<div data-block="true"><div>6.2 06:00 - 08:00, 11:00 - 13:00, 16:00 - 18:00, 21:00 - 23:00</div></div>
+`
+
+	result, found := parseScheduleFromParagraphs(html)
+
+	require.True(t, found, "div parser should find schedule rows")
+	assert.Equal(t, "00:00 - 02:00, 06:00 - 08:00", result.Col1_1)
+	assert.Equal(t, "08:00 - 10:00, 13:00 - 15:00", result.Col2_1)
+	assert.Equal(t, "06:00 - 08:00, 11:00 - 13:00, 16:00 - 18:00, 21:00 - 23:00", result.Col6_2)
+}
+
+func TestHasScheduleData(t *testing.T) {
+	assert.False(t, hasScheduleData(models.Schedule{}), "empty schedule should be treated as missing")
+	assert.True(t, hasScheduleData(models.Schedule{Col3_2: "01:00 - 02:00"}), "any non-empty column should count")
+	assert.True(t, hasScheduleData(models.Schedule{Col1_1: " 01:00 - 02:00 "}), "trimming should still detect data")
 }
