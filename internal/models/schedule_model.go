dda0d44af20074915a6078117c1385efc6f84416
sigmanor <sigmanor@users.noreply.github.com>
2025-12-07 13:52:51 +0200

    fix(models): map schedule_date column

--- internal/models/schedule_model.go
+++ internal/models/schedule_model.go
@@ -7,19 +7,19 @@ type Schedule struct {
 	NewsID       int       `gorm:"uniqueIndex" json:"news_id"`
 	Title        string    `gorm:"type:text" json:"title"`
 	Date         time.Time `gorm:"not null" json:"date"`
-	ScheduleDate string    `gorm:"-" json:"schedule_date"`
+	ScheduleDate string    `gorm:"column:schedule_date" json:"schedule_date"`
 	Col1_1       string    `gorm:"column:1_1" json:"1_1"`
-	Col1_2 string    `gorm:"column:1_2" json:"1_2"`
-	Col2_1 string    `gorm:"column:2_1" json:"2_1"`
-	Col2_2 string    `gorm:"column:2_2" json:"2_2"`
-	Col3_1 string    `gorm:"column:3_1" json:"3_1"`
-	Col3_2 string    `gorm:"column:3_2" json:"3_2"`
-	Col4_1 string    `gorm:"column:4_1" json:"4_1"`
-	Col4_2 string    `gorm:"column:4_2" json:"4_2"`
-	Col5_1 string    `gorm:"column:5_1" json:"5_1"`
-	Col5_2 string    `gorm:"column:5_2" json:"5_2"`
-	Col6_1 string    `gorm:"column:6_1" json:"6_1"`
-	Col6_2 string    `gorm:"column:6_2" json:"6_2"`
+	Col1_2       string    `gorm:"column:1_2" json:"1_2"`
+	Col2_1       string    `gorm:"column:2_1" json:"2_1"`
+	Col2_2       string    `gorm:"column:2_2" json:"2_2"`
+	Col3_1       string    `gorm:"column:3_1" json:"3_1"`
+	Col3_2       string    `gorm:"column:3_2" json:"3_2"`
+	Col4_1       string    `gorm:"column:4_1" json:"4_1"`
+	Col4_2       string    `gorm:"column:4_2" json:"4_2"`
+	Col5_1       string    `gorm:"column:5_1" json:"5_1"`
+	Col5_2       string    `gorm:"column:5_2" json:"5_2"`
+	Col6_1       string    `gorm:"column:6_1" json:"6_1"`
+	Col6_2       string    `gorm:"column:6_2" json:"6_2"`
 }
 
 func (d *Schedule) FormatDate() string {
