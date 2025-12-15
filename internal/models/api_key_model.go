43189da753fc081150666de989df635e7945f9e9
sigmanor <sigmanor@users.noreply.github.com>
2025-12-15 14:44:29 +0200

    feat(models): add webhook fields to api key

--- internal/models/api_key_model.go
+++ internal/models/api_key_model.go
@@ -4,6 +4,9 @@ import "gorm.io/gorm"
 
 type APIKey struct {
 	gorm.Model
-	Key       string `gorm:"uniqueIndex" json:"key"`
-	RateLimit int    `json:"rate_limit"`
+	Key                string  `gorm:"uniqueIndex" json:"key"`
+	RateLimit          int     `json:"rate_limit"`
+	WebhookURL         string  `gorm:"type:varchar(255)" json:"webhook_url"`
+	WebhookEnabled     bool    `gorm:"default:false" json:"webhook_enabled"`
+	WebhookFailedAttempts int  `gorm:"default:0" json:"webhook_failed_attempts"`
 }
