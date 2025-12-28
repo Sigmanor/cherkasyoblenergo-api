d57ecd62883ccf754e2ff7677414b51e956b2480
sigmanor <sigmanor@users.noreply.github.com>
2025-12-28 13:34:56 +0200

    feat(logger): add global logger setup

---
+++ internal/logger/global.go
@@ -0,0 +1,30 @@
+package logger
+
+import (
+	"log/slog"
+	"os"
+	"strings"
+)
+
+func SetupGlobal(level string) {
+	var logLevel slog.Level
+
+	switch strings.ToLower(level) {
+	case "debug":
+		logLevel = slog.LevelDebug
+	case "info":
+		logLevel = slog.LevelInfo
+	case "warn", "warning":
+		logLevel = slog.LevelWarn
+	case "error":
+		logLevel = slog.LevelError
+	default:
+		logLevel = slog.LevelInfo
+	}
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		Level: logLevel,
+	}))
+
+	slog.SetDefault(logger)
+}
